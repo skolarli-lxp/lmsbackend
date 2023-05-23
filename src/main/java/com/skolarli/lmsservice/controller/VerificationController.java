3320094c79dba37042d318fd17ca1ed0fbc7a734
skolarli-dev <skolarli@outlook.com>
2023-05-23 21:45:56 +0530

    Added endpoint to regenerate token

--- src/main/java/com/skolarli/lmsservice/controller/VerificationController.java
+++ src/main/java/com/skolarli/lmsservice/controller/VerificationController.java
@@ -1,16 +1,18 @@
 package com.skolarli.lmsservice.controller;
 
+import com.skolarli.lmsservice.authentications.TenantAuthenticationToken;
+import com.skolarli.lmsservice.models.db.LmsUser;
+import com.skolarli.lmsservice.models.db.Tenant;
 import com.skolarli.lmsservice.models.db.VerificationCode;
+import com.skolarli.lmsservice.services.LmsUserService;
+import com.skolarli.lmsservice.services.TenantService;
 import com.skolarli.lmsservice.services.VerificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
@@ -20,8 +22,19 @@ public class VerificationController {
 
     final Logger logger = LoggerFactory.getLogger(VerificationController.class);
 
-    @Autowired
-    private VerificationService verificationService;
+    final VerificationService verificationService;
+
+    final LmsUserService lmsUserService;
+
+    final TenantService tenantService;
+
+    public VerificationController(VerificationService verificationService,
+                                  LmsUserService lmsUserService,
+                                  TenantService tenantService) {
+        this.verificationService = verificationService;
+        this.lmsUserService = lmsUserService;
+        this.tenantService = tenantService;
+    }
 
     @GetMapping(value = "/verifytoken")
     public ResponseEntity<String> verifyToken(@RequestParam String token) {
@@ -38,6 +51,18 @@ public class VerificationController {
     @GetMapping(value = "/gettoken")
     public ResponseEntity<VerificationCode> getToken(@RequestParam Long userId) {
         VerificationCode code = verificationService.getVerificationCodeByUser(userId);
+        return new ResponseEntity<>(code, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/regeneratetoken")
+    public ResponseEntity<VerificationCode> generateToken(@RequestParam Long userId,
+                                                          @RequestHeader("Domain")
+                                                          String domainName) {
+        Tenant savedTenant = tenantService.getTenantByDomainName(domainName);
+        SecurityContextHolder.getContext().setAuthentication(
+                new TenantAuthenticationToken("", savedTenant.getId()));
+        LmsUser user = lmsUserService.getLmsUserById(userId);
+        VerificationCode code = verificationService.generateAndSaveVerificationCode(user);
         return new ResponseEntity<>(code, HttpStatus.OK);
     }
 }
