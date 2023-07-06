01e25c2719469c08a1727362a639c5513f4c3dbc
skolarli-dev <skolarli@outlook.com>
2023-07-06 14:31:50 +0530

    Added authroixation token to swagger UI

--- src/main/java/com/skolarli/lmsservice/config/SwaggerConfig.java
+++ src/main/java/com/skolarli/lmsservice/config/SwaggerConfig.java
@@ -7,13 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -26,7 +30,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage(
                         "com.skolarli.lmsservice.controller"))
                 .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiInfoMetaData());
+                .build()
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .apiInfo(apiInfoMetaData());
     }
 
     private ApiInfo apiInfoMetaData() {
@@ -42,6 +49,33 @@ public class SwaggerConfig implements WebMvcConfigurer {
         return UiConfigurationBuilder
                 .builder()
                 .defaultModelsExpandDepth(-1)
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(securityReference()))
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityReference securityReference() {
+        AuthorizationScope[] authorizationScopes = {
+                new AuthorizationScope("global", "accessEverything")
+        };
+        return SecurityReference.builder()
+                .reference("Authorization")
+                .scopes(authorizationScopes)
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    @Bean
+    public SecurityConfiguration securityConfiguration() {
+        return SecurityConfigurationBuilder.builder()
                 .build();
     }
 }
